42933d948bc5849fb49c4fe32459d6760ba85b65
Parth Patel <parthcdgsa@gmail.com>
2019-02-22 14:52:38 +0530

    optimize images

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,7 @@
 const wrapper = document.querySelector('.hamburger');
 const menu = document.querySelector('.hamburger__menu');
 const navigation = document.querySelector('.navigation');
+const navigationLink = document.querySelectorAll('.navigation__link');
 const container = document.querySelector('.container');
 const monthHolder = document.querySelector('#daymonth');
 const yearHolder = document.querySelector('#year');
@@ -19,4 +20,17 @@ wrapper.addEventListener('click', function(){
     menu.classList.toggle('animate');
     navigation.classList.toggle('is-visible');
     container.classList.toggle('active');
+});
+
+navigationLink.forEach(element => {
+    element.addEventListener('click', function(){
+        menu.classList.toggle('animate');
+        navigation.classList.toggle('is-visible');
+        container.classList.toggle('active');
+    });
+});
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    offset: 40
 });
